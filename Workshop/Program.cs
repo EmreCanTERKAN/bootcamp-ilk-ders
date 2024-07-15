0636f0cf446f7424918967442f64521b8a780ef7
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-07-15 14:17:25 +0300

    adsoyad ve tercih sebebimin olduğu
    15.07.2024 C#ta ilk uygulaması

---
+++ Workshop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlTypes;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Emre Can TERKAN");
+        Console.WriteLine("Full stack bir yazılımcı olmak için bir bootcamp'e ihtiyacım vardı. Araştırma sonucunda tercihimi Patikadan yana kullandım. Herkese başarılar  ");
+
+
+    }
+}
